b28d4ccd46fdd93a20e2e9df5729176380c23ab9
Elierrocha <33941093+Elierrocha@users.noreply.github.com>
2017-11-25 17:13:53 -0600

    Pagina Principal del Sistema Web
    Esta es la pagina principal del sistema web, desde

---
+++ index.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Uniservices</title>
+    <link rel="shortcut icon" href="../imagenes/logoUNI.ico" type="image/x-icon">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/estilo.css" rel="stylesheet">
+    <link href="css/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <script src="js/jquery.js"></script>
+    <script src="js/back-to-top1.js"></script>
+    <script src="js/bootstrap.js"></script>
+
+</head>
+<body>
+        <?php include ('includes/menu.php'); ?>   <!-- Menu -->
+       <br><br><br>
+        <div class="container">
+            <div class="row">
+              <!-- Parte Superior -->
+                <div class="col-lg-12">
+                        <div class="col-md-3"><img src="imagenes/logo.png" width="80" height="80" class="img-responsive"></div>
+                             <div class="col-md-6">                      
+                               <h2>Sistema Administrativo Uniservices</h2>                   
+                        </div>
+                        <div class="col-md-3">
+                      <img class="img-responsive img-circle" src="imagenes/logo.png" width="50px" height="50px">
+                      <h5><i class="fa fa-circle fa-stack-1x fa-inverse" style="color:green; text-align: left; "></i><b> &nbsp; Online: </b>Juan Perez</h5>
+                        </div> 
+                </div>
+        </div>
+        <!-- Contenido Central -->
+   <hr>
+        <div class="row">
+                      <div class="col-md-3">
+                         <div class="card">
+                            <!--Imagen-->
+                            <div class="view overlay hm-white-slight hm-zoom hoverable">
+                                <img src="imagenes/cliente.jpeg" class="img-fluid" alt="">
+                                <a href="#">
+                                    <div class="mask waves-effect waves-light"></div>
+                                </a>
+                            </div>
+
+                            <!--Contenido-->
+                            <div class="card-body">
+                                <p class="cantidadRegistros">Total Registros: 34</p>
+                                <p class="card-text text-truncate">Mantenimientos de los clientes.</p>
+                                <h5 class="card-title">Realizacion de todas las operaciones que se pueden realizar en un sistema</h5>
+                                <a href="#" class="btnHover botonNaranja">Ir al curso</a>
+                            </div>
+                        </div>
+                    </div> 
+
+                     <div class="col-md-3">
+                         <div class="card">
+                            <!--Imagen-->
+                            <div class="view overlay hm-white-slight hm-zoom hoverable">
+                                <img src="imagenes/iconoUniservice.png" class="img-fluid" alt="">
+                                <a href="#">
+                                    <div class="mask waves-effect waves-light"></div>
+                                </a>
+                            </div>
+
+                            <!--Contenido-->
+                            <div class="card-body">
+                                <p class="cantidadRegistros">Total Registros: 34</p>
+                                <p class="card-text text-truncate">Mantenimientos de los clientes.</p>
+                                <h5 class="card-title">Realizacion de todas las operaciones que se pueden realizar en un sistema</h5>
+                                <a href="#" class="botonNaranja">Ir al curso</a>
+                            </div>
+                        </div>
+                    </div> 
+                      <div class="col-md-3">
+                         <div class="card">
+                            <!--Imagen-->
+                            <div class="view overlay hm-white-slight hm-zoom hoverable">
+                                <img src="imagenes/iconoUniservice.png" class="img-fluid" alt="">
+                                <a href="#">
+                                    <div class="mask waves-effect waves-light"></div>
+                                </a>
+                            </div>
+
+                            <!--Contenido-->
+                            <div class="card-body">
+                                <p class="cantidadRegistros">Total Registros: 34</p>
+                                <p class="card-text text-truncate">Mantenimientos de los clientes.</p>
+                                <h5 class="card-title">Realizacion de todas las operaciones que se pueden realizar en un sistema</h5>
+                                <a href="#" class="botonNaranja">Ir al curso</a>
+                            </div>
+                        </div>
+                    </div> 
+                      <div class="col-md-3">
+                         <div class="card">
+                            <!--Imagen-->
+                            <div class="view overlay hm-white-slight hm-zoom hoverable">
+                                <img src="imagenes/iconoUniservice.png" class="img-fluid" alt="">
+                                <a href="#">
+                                    <div class="mask waves-effect waves-light"></div>
+                                </a>
+                            </div>
+
+                            <!--Contenido-->
+                            <div class="card-body">
+                                <p class="cantidadRegistros">Total Registros: 34</p>
+                                <p class="card-text text-truncate">Mantenimientos de los clientes.</p>
+                                <h5 class="card-title">Realizacion de todas las operaciones que se pueden realizar en un sistema</h5>
+                                <a href="#" class="botonNaranja">Ir al curso</a>
+                            </div>
+                        </div>
+                    </div> 
+                      <div class="col-md-3">
+                         <div class="card">
+                            <!--Imagen-->
+                            <div class="view overlay hm-white-slight hm-zoom hoverable">
+                                <img src="imagenes/iconoUniservice.png" class="img-fluid" alt="">
+                                <a href="#">
+                                    <div class="mask waves-effect waves-light"></div>
+                                </a>
+                            </div>
+
+                            <!--Contenido-->
+                            <div class="card-body">
+                                <p class="cantidadRegistros">Total Registros: 34</p>
+                                <p class="card-text text-truncate">Mantenimientos de los clientes.</p>
+                                <h5 class="card-title">Realizacion de todas las operaciones que se pueden realizar en un sistema</h5>
+                                <a href="#" class="botonNaranja">Ir al curso</a>
+                            </div>
+                        </div>
+                    </div> 
+                      <div class="col-md-3">
+                         <div class="card">
+                            <!--Imagen-->
+                            <div class="view overlay hm-white-slight hm-zoom hoverable">
+                                <img src="imagenes/iconoUniservice.png" class="img-fluid" alt="">
+                                <a href="#">
+                                    <div class="mask waves-effect waves-light"></div>
+                                </a>
+                            </div>
+
+                            <!--Contenido-->
+                            <div class="card-body">
+                                <p class="cantidadRegistros">Total Registros: 34</p>
+                                <p class="card-text text-truncate">Mantenimientos de los clientes.</p>
+                                <h5 class="card-title">Realizacion de todas las operaciones que se pueden realizar en un sistema</h5>
+                                <a href="#" class="botonNaranja">Ir al curso</a>
+                            </div>
+                        </div>
+                    </div> 
+                      <div class="col-md-3">
+                         <div class="card">
+                            <!--Imagen-->
+                            <div class="view overlay hm-white-slight hm-zoom hoverable">
+                                <img src="imagenes/iconoUniservice.png" class="img-fluid" alt="">
+                                <a href="#">
+                                    <div class="mask waves-effect waves-light"></div>
+                                </a>
+                            </div>
+
+                            <!--Contenido-->
+                            <div class="card-body">
+                                <p class="cantidadRegistros">Total Registros: 34</p>
+                                <p class="card-text text-truncate">Mantenimientos de los clientes.</p>
+                                <h5 class="card-title">Realizacion de todas las operaciones que se pueden realizar en un sistema</h5>
+                                <a href="#" class="botonNaranja">Ir al curso</a>
+                            </div>
+                        </div>
+                    </div> 
+
+                      <div class="col-md-3">
+                         <div class="card">
+                            <!--Imagen-->
+                            <div class="view overlay hm-white-slight hm-zoom hoverable">
+                                <img src="imagenes/iconoUniservice.png" class="img-fluid" alt="">
+                                <a href="#">
+                                    <div class="mask waves-effect waves-light"></div>
+                                </a>
+                            </div>
+
+                            <!--Contenido-->
+                            <div class="card-body">
+                                <p class="cantidadRegistros">Total Registros: 34</p>
+                                <p class="card-text text-truncate">Mantenimientos de los clientes.</p>
+                                <h5 class="card-title">Realizacion de todas las operaciones que se pueden realizar en un sistema</h5>
+                                <a href="#" class="botonNaranja">Ir al curso</a>
+                            </div>
+                        </div>
+                    </div> 
+        </div>
+
+        <!-- Final del Contenido Central -->
+    </div>
+    <?php
+    include('includes/footer.php');
+ ?>
+</body>
+</html>
